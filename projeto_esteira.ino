50fd286ffb20790f7cad7654a5c76af71f9805be
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2022-09-24 23:24:12 -0300

    sensor control, trash identification and cycle

--- projeto_esteira.ino
+++ projeto_esteira.ino
@@ -1,20 +1,21 @@
 class Motor{
-  int control_pin;
+  private:
+  int _control_pin;
 
   public:
   Motor(int pin){
     Serial.println("Montando o motor");
-    control_pin = pin;
+    _control_pin = pin;
     pinMode(pin,OUTPUT);
   }
   
   void turnOn(){
     Serial.println("Ligando o motor");
-    digitalWrite(control_pin,HIGH);
+    digitalWrite(_control_pin,HIGH);
   }
   void shutDown(){
     Serial.println("Desligando o motor");
-    digitalWrite(control_pin,LOW);
+    digitalWrite(_control_pin,LOW);
   }
 
   void setSpeed(int vel){
@@ -24,48 +25,173 @@ class Motor{
 };
 
 class Sensor{
-  int sensor_pin;
-  String name;
+  private:
+  int _sensor_pin;
+  String _name;
   
   public:
-  Sensor(int pin){
-    Serial.println("Montando sensor");
+  
+  Sensor(int pin, String name){
     pinMode(pin, INPUT);
-    sensor_pin = pin;
-  }
-
-  void defineName(String name){
-    Serial.println("Definindo nome do sensor para " + name);
-    this->name = name;
+    _sensor_pin = pin;
+    _name = name;
   }
   
   bool read(){
-    //Serial.println("Lendo sensor " + name);
-    return digitalRead(sensor_pin);
+    //Serial.println("Lendo sensor " + _name);
+    return digitalRead(_sensor_pin);
   }
 };
 
-boolean state = false;
-Motor motor(8);
-Sensor sensor(2);
+class OpticalSensor : public Sensor{
+  
+  public:
+    OpticalSensor(int pin, String name) : Sensor(pin, name){
+    }
+};
+
+class IDSensor : public Sensor{
+  
+  public:
+  String _material;
+
+  IDSensor(int pin, String name, String material) : Sensor(pin, name){
+    _material = material;
+  }
+};
+
+#define SENSOR_COUNT 3
+
+class SensorsArray{
+  private:
+    int _count;
+    
+  public:
+    bool states[SENSOR_COUNT];
+    virtual void checkAll();   
+    
+    void resetState(){
+      for(int i = 0; i < SENSOR_COUNT; i++){
+        states[i] = false;
+      }
+    }
+
+    bool getSensorState(int i){
+      return states[i];
+    }
+
+    int getCount(){
+      return _count;
+    }
+
+    void setCount(int val){
+      _count = val;
+    }
+};
+
+class IDSensorArray : public SensorsArray{
+
+  private:
+  IDSensor* sensors[SENSOR_COUNT];
+  
+  public:
+  IDSensorArray() : SensorsArray(){}
+
+  void addNew(IDSensor* sensor){
+    if(getCount() < SENSOR_COUNT){
+      sensors[getCount()] = sensor;
+      setCount(getCount() + 1); 
+    }
+  }
+
+  void checkAll(){
+      for(int i = 0; i < getCount(); i++){
+        bool sensorState = sensors[i]->read();
+
+        if(sensorState){
+          states[i] = sensorState;
+        }
+      }
+    } 
+  
+  String getMaterial(){
+    for(int i = 0; i < SENSOR_COUNT; i++){
+      getSensorState(i);
+      if(getSensorState(i)){
+        return sensors[i]->_material;
+      } 
+    }
+    return "NONE";
+  }
+  
+};
+
+class OPSensorArray : public SensorsArray{
+  private:
+  OpticalSensor* sensors[SENSOR_COUNT];
+
+  public:
+  void addNew(OpticalSensor* sensor){
+    if(getCount() < SENSOR_COUNT){
+      sensors[getCount()] = sensor;
+      setCount(getCount() + 1);
+    }
+  }
+
+    void checkAll(){
+      for(int i = 0; i < getCount(); i++){
+        bool sensorState = sensors[i]->read();
+
+        if(sensorState){
+          states[i] = sensorState;
+        }
+      }
+    } 
+};
+
+#define MOTOR_PIN 8
+
+#define IND_SENS_PIN 2
+#define CAP_SENS_PIN 3
+#define CAP_SENS_PIN_2 4
+
+#define OP_SENS_PIN 5
+#define OP_SENS_PIN_2 6
+
+bool cycleReady = true;
+
+Motor motor(MOTOR_PIN);
+
+IDSensor sensorInd(IND_SENS_PIN, "INDUTIVO","METAL");
+IDSensor sensorCap1(CAP_SENS_PIN, "CAPACITIVO 1","VIDRO");
+IDSensor sensorCap2(CAP_SENS_PIN_2, "CAPACITIVO 2","PLASTICO");
+
+OpticalSensor sensorOP1(OP_SENS_PIN, "OPTICO FINAL");
+OpticalSensor sensorOP2(OP_SENS_PIN_2, "OPTICO METAL");
+
+IDSensorArray IDsensors;
 
 void setup()
 {
   Serial.begin(9600);
-  sensor.defineName("CAPACITIVO");
+  
+  IDsensors.addNew(&sensorCap1);
+  IDsensors.addNew(&sensorCap2);
+  IDsensors.addNew(&sensorInd);
 }
 
 void loop()
 {
-  if(sensor.read()){
-    state = !state;
-    
-    if(state){
-      motor.turnOn();
-      delay(250);
-    }else{
-      motor.shutDown();
-      delay(250);
+  if(cycleReady){
+    IDsensors.checkAll(); 
+    if(sensorOP1.read()){
+      Serial.println(IDsensors.getMaterial());
+      IDsensors.resetState();
+      cycleReady = false;
     }
+  }else{
+    if(sensorOP2.read()){
+      cycleReady = true;
+    } 
   }
 }
